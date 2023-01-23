f81eb68d3e85d53a204d35ecf5b84d154ca9ce2c
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-01-23 09:34:36 -0800

    next defaults cleanup

---
+++ components/seo/seo.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function SEO() {
+  return (
+    <Head>
+      <title>Welcome to Pashion Pilates</title>
+      <meta
+        name="description"
+        content="Pashion Pilates is a pilates subscription website, with instructors Sheila and Pari Hosseini"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
